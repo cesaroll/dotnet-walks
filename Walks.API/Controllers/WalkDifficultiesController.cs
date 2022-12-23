7b7d3f8cd9088dbf9a4065535774153e131ef398
Cesar Lopez <cesarlerdeut@gmail.com>
2022-12-23 17:30:11 +0100

    Add WalkDifficultiesController

---
+++ Walks.API/Controllers/WalkDifficultiesController.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Mvc;
+using Walks.API.Models.DTOs.Requests;
+using Walks.API.Services;
+
+namespace Walks.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WalkDifficultiesController : Controller
+{
+    private readonly IWalkDifficultyService _walkDifficultyService;
+
+    public WalkDifficultiesController(IWalkDifficultyService walkDifficultyService)
+    {
+        _walkDifficultyService = walkDifficultyService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllWalkDifficultiesAsync()
+    {
+        var walkDifficulties = await _walkDifficultyService.GetAllAsync();
+        return Ok(walkDifficulties);
+    }
+
+    [HttpGet]
+    [Route("{id:guid}")]
+    [ActionName("GetWalkDifficultyAsync")]
+    public async Task<IActionResult> GetWalkDifficultyAsync(Guid id)
+    {
+        var walkDifficulty = await _walkDifficultyService.GetAsync(id);
+        return Ok(walkDifficulty);
+    }
+
+    [HttpDelete]
+    [Route("{id:guid}")]
+    public async Task DeleteWalkDifficultyAsync(Guid id)
+    {
+        await _walkDifficultyService.DeleteAsync(id);
+    }
+
+    [HttpPost]
+    [Route("{id:guid}")]
+    public async Task<IActionResult> AddWalkDifficultyAsync(MutateWalkDifficultyRequest mutateWalkDifficultyRequest)
+    {
+        var walkDifficulty = await _walkDifficultyService.AddAsync(mutateWalkDifficultyRequest);
+
+        return CreatedAtAction(
+            nameof(GetWalkDifficultyAsync), 
+            new {Id = walkDifficulty.Id}, 
+            walkDifficulty);
+    }
+    
+    [HttpPut]
+    [Route("{id:guid}")]
+    public async Task<IActionResult> UpdateWalkDifficultyAsync(
+        [FromRoute] Guid id,
+        [FromBody] MutateWalkDifficultyRequest mutateWalkDifficultyRequest)
+    {
+        var walkDifficulty = await _walkDifficultyService.UpdateAsync(id, mutateWalkDifficultyRequest);
+        return Ok(walkDifficulty);
+    }
+}
--- Walks.API/Models/DTOs/Requests/MutateWalkDifficultyRequest.cs
+++ Walks.API/Models/DTOs/Requests/MutateWalkDifficultyRequest.cs
@@ -2,5 +2,5 @@ namespace Walks.API.Models.DTOs.Requests;
 
 public record MutateWalkDifficultyRequest
 {
-    
+    public string Code { get; init; } = string.Empty;
 }
